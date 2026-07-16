cc2b5c23c4ba4739d756c81ba8c423f719a15a09
MaeGobi <maelis.gobinet@univ-amu.fr>
2026-07-16 17:23:42 +0200

    Add function NaN imputation

--- template_teaching_project_ml_OFF/notebooks/Missing data imputation.py
+++ template_teaching_project_ml_OFF/notebooks/Missing data imputation.py
@@ -1,8 +1,16 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import matplotlib.colors as mcolors
+import sklearn as sk
+import seaborn as sns
+from sklearn.impute import SimpleImputerImputer
 from sklearn.impute import KNNImputer
 from sklearn.preprocessing import StandardScaler
 
 def imputation():
+    # Imputation des variables numériques par KNN
     numeric_features = df.select_dtypes(include=['float','int'])
     numeric_features.shape
 
@@ -22,6 +30,31 @@ def imputation():
         axes[1].set_title(f"Imputed {feature}");
     compare_dist("LotFrontage")
 
+    # Imputation des variables catégorielles par le mode
+    categorical_features = df.select_dtypes(include=['object','category'])
+    categorical_features.shape
 
-    if __name__ == "__main__":
+    from sklearn.impute import SimpleImputer
+    imputation_cat = SimpleImputer(missing_values=np.nan, strategy='most_frequent')
+    imputed_cat = imputation_cat.fit_transform(categorical_features)
+    imputed_cat.shape
+
+    df.loc[:, categorical_features.columns] = imputed_cat
+
+    def compare_dist_cat(feature):
+        fig, axes = plt.subplots(1, 2, figsize=(12, 3))
+        sns.countplot(x=raw_df.loc[:, feature], ax=axes[0])
+        axes[0].set_title(f"Raw {feature}")
+        axes[0].tick_params(axis='x', rotation=45)
+
+        sns.countplot(x=df.loc[:, feature], ax=axes[1])
+        axes[1].set_title(f"Imputed {feature}")
+        axes[1].tick_params(axis='x', rotation=45)
+        plt.tight_layout()
+
+    compare_dist_cat("nom_de_votre_variable_categorielle")
+    
+    
+
+if __name__ == "__main__":
         imputation()
